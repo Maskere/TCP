90eeea552a53e88da7cda2bbdab3d5cb010693ac
Maskere <Mstachkruse@gmail.com>
2025-03-07 21:29:46 +0100

    Refactoring

--- obj/Debug/net8.0/TCP.AssemblyInfo.cs
+++ obj/Debug/net8.0/TCP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495aebbabd3ae5adb6332cab86f542174c757854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f70fec870fad186e91d3b8ac6e79c3735c1c6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
