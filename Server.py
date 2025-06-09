aa143fcf4b7c38a4bd6cb4e076c71fdbbd7900b7
Miki-Aqqalu Kruse <mstachkruse@gmail.com>
2025-06-09 19:49:20 +0200

    Add files via upload

--- Server.py
+++ Server.py
@@ -1,29 +1,52 @@
-from socket import * 
+from socket import *
+import json
 from threading import Thread
 
-def HandleClient(connectionSocket):
+def HandleClient(connectionSocket,games = []):
+    if games is not None:
+        print(games)
     try:
         while True:
-            sentence = connectionSocket.recv(1024)
-            if not sentence:
+            request = connectionSocket.recv(1024).decode()
+            print(request)
+            if not request:
                 break
-            capitilizedSentence = sentence.upper()
-            connectionSocket.send(capitilizedSentence)
+            if request == "all\r\n":
+                connectionSocket.send(json.dumps(games).encode())
+            elif request == "add\r\n":
+                connectionSocket.send("Insert values".encode())
+                request = json.loads(connectionSocket.recv(1024).decode())
+                games.append(request)
+                connectionSocket.send(json.dumps(games).encode())
     finally:
         connectionSocket.close()
 
+games = [
+    {"id":1,"name":"Pandemic","score":9.2},
+    {"id":2,"name":"7 Wonders","score":6.2},
+    {"id":3,"name":"Catan","score":8.5},
+    {"id":4,"name":"Wingspan","score":7.3},
+]
 
-serverName = "0.0.0.0"
-serverPort = 13000
+serverName = "localhost"
+serverPort = 22334
 
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind((serverName,serverPort))
-serverSocket.listen()
-print("Server initiated")
+serverSocket.listen(1)
+serverSocket.settimeout(1)
 
-while True:
-    connectionSocket, clientAddr = serverSocket.accept()
-    print(f"Connection established with {clientAddr}")
+try:
+    while True:
+        try:
+            connectionSocket, clientAddr = serverSocket.accept()
+            print(f"Connection estrablished with {clientAddr}")
 
-    client_thread = Thread(target=HandleClient, args=(connectionSocket,))
-    client_thread.start()
+            client_thread = Thread(target=HandleClient, args=(connectionSocket,games,))
+            client_thread.start()
+        except timeout:
+            continue
+except KeyboardInterrupt:
+    print("Server is shutting down")
+finally:
+    serverSocket.close()
